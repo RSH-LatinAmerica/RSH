2ed5ba015eb5b220c4fec7696eea83db838aa1b5
Edenilson Lara <74226677+elara066@users.noreply.github.com>
2024-05-08 19:49:17 -0600

    First Version

---
+++ GIT/RSH-Space/Onboarding/src_dataClasses/onboarding/Data.java
@@ -0,0 +1,32 @@
+package onboarding;
+
+/**
+ */
+@SuppressWarnings("all")
+@javax.annotation.processing.Generated(comments="This is the java file of the ivy data class Data", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class Data extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 7765810832485990537L;
+
+  private java.lang.String empresa;
+
+  /**
+   * Gets the field empresa.
+   * @return the value of the field empresa; may be null.
+   */
+  public java.lang.String getEmpresa()
+  {
+    return empresa;
+  }
+
+  /**
+   * Sets the field empresa.
+   * @param _empresa the new value of the field empresa.
+   */
+  public void setEmpresa(java.lang.String _empresa)
+  {
+    empresa = _empresa;
+  }
+
+}
